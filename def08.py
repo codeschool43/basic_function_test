313ae34611ec4ac5cff2a481d4a8c46eae36ff19
Jalilov <javohirjalilov777@gmail.com>
2021-12-02 23:51:02 +0500

    added test solution

---
+++ def08.py
@@ -0,0 +1,13 @@
+# Create function to find Pi to 4 number of decimal places.
+def main():
+    """
+    Calculate Pi to n number of decimal places.
+    """
+    #Calculate Pi to n number of decimal places.
+    n = 4
+    #Create variable to store Pi.
+    pi = 3.1415926535897931159979634685441852
+    #Return Pi to n number of decimal places.
+    return round(pi, n)
+
+print(main())
